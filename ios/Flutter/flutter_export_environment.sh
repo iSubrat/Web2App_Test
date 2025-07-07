188661184b8ddcd5c4da70b0e630f2e22d416b22
Subrat Gupta <isubrat@Subrats-MacBook-Air.local>
2025-07-07 13:39:26 +0530

    updated to latest version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter_windows_3.29.3-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\webly_flutter"
+export "FLUTTER_ROOT=/Users/isubrat/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/isubrat/AndroidStudioProjects/webly_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
